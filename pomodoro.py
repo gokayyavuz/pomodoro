668f601592867c6604cb95f93c3d8bcb3ee5b97d
goekayyavuz <goekay.yavuz@outlook.com>
2025-03-14 23:16:32 +0100

    Initial commit

--- pomodoro.py
+++ pomodoro.py
@@ -1,12 +1,16 @@
 import time
 import datetime
+import streamlit as st
 
 # print(time.localtime())
 
 x = 10
 
+# Timer
 for i in range(x):
     secondi = x - i - 1
     print(secondi)
     print(datetime.timedelta(seconds=secondi))
     time.sleep(1)
+
+# Auswahlmöglichkeit wie die Zyklen aussehen
